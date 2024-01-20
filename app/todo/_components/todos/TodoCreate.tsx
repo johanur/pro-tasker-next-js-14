930199089bae2c5c579f27f5e2fd190168e1b606
Johan <johanur.rahman@pm.me>
2024-01-21 04:05:21 +0600

    Todo create form added

--- app/todo/_components/todos/TodoCreate.tsx
+++ app/todo/_components/todos/TodoCreate.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Dialog,
   DialogContent,
@@ -7,25 +9,130 @@ import {
   DialogClose,
   DialogFooter,
 } from '@/components/ui/dialog';
+import { Calendar as CalendarIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+
+import * as z from "zod"
+import { cn } from '@/lib/utils';
+import { addDays, format, isBefore } from 'date-fns';
+import { TodoSchema } from '@/app/todo/_schema/todo.schema';
+import { useState } from 'react';
+
 const TodoCreate = ({ isOpen, onToggle }: any) => {
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+
+  const oneDayBefore = addDays(new Date(), -1);
+
+  const form = useForm<z.infer<typeof TodoSchema>>({
+    resolver: zodResolver(TodoSchema),
+  })
+
+  function onSubmit(values: z.infer<typeof TodoSchema>) {
+    console.log(values)
+  }
   const handleCloseDialog = () => {
+    form.reset();
     onToggle(false);
   };
+
   return (
     <>
       <Dialog open={isOpen} onOpenChange={handleCloseDialog}>
+
         <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your account and remove your data from our
-              servers.
-            </DialogDescription>
-
-            <DialogFooter className="sm:justify-start">
-
-            </DialogFooter>
+          <DialogHeader className="mb-5">
+            <DialogTitle>Add Todo</DialogTitle>
           </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter a title..." {...field} />
+                      </FormControl>
+                    </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea placeholder="Add a more details description..." {...field} />
+                      </FormControl>
+                    </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="expiryDate"
+                render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Expiry Date</FormLabel>
+                      <Popover open={isCalendarOpen} onOpenChange={setIsCalendarOpen}>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "pl-3 flex justify-start font-normal w-full",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              <CalendarIcon className="mr-2 h-4 w-4" />
+                              {field.value ? (
+                                format(field.value, 'PPP')
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <div className="flex m-1">
+                            <div className="flex-1"></div>
+                            </div>
+
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={(e) => { field.onChange(e); setIsCalendarOpen(false); } }
+                            disabled={(date) => isBefore(date, oneDayBefore)}
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                    </FormItem>
+                )}
+              />
+              <Button type="submit" className="bg-indigo-600 h-8 rounded-sm w-1/5">Submit</Button>
+              <Button type="button" variant="secondary" className="mx-4 h-8 rounded-sm w-1/5" onClick={handleCloseDialog}>Close</Button>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </>
---
+++ app/todo/_schema/todo.schema.ts
@@ -0,0 +1,20 @@
+import * as z from 'zod';
+import { ZodType } from 'zod';
+import { TodoFormData } from '@/app/todo/_types/todo';
+
+export const TodoSchema:  ZodType<TodoFormData> = z.object({
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title is required",
+  }).min(3, {
+    message: "Title is too short."
+  }),
+  description: z.string({
+    required_error: "Description is required",
+    invalid_type_error: "Description is required",
+  }),
+  expiryDate: z.date({
+    required_error: "Expiry date is required",
+    invalid_type_error: "Expiry date is required",
+  })
+})
