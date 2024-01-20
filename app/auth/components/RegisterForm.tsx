bad02d8227015e18f0544d847e24d6643b3384a4
Johan <johanur.rahman@pm.me>
2024-01-20 13:26:19 +0600

    Register form updated with validation and post to server functionality

--- app/auth/components/RegisterForm.tsx
+++ app/auth/components/RegisterForm.tsx
@@ -1,27 +1,53 @@
+'use client';
+
+import { z } from 'zod';
 import Link from 'next/link';
 import styles from '../styles/auth.module.scss';
+import { RegisterSchema } from '@/app/auth/schema/register.schema';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { RegisterFormData } from '@/app/auth/types/form';
+import { registerWithEmailAndPassword } from '@/app/auth/actions/register.action';
+
+type Inputs = z.infer<typeof RegisterSchema>;
 
 const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(RegisterSchema),
+  });
+
+  const submitData = async (data: RegisterFormData) => {
+    const result = await registerWithEmailAndPassword(data);
+
+    // TODO: While Submitting - Disable Input fields and submit button. If possible add spinner to submit button
+    // TODO: Handle Error & Success - Show Toastr
+  };
+
   return (
     <div className={styles['authentication-container']}>
       <div className={styles.header}>
         <img
           className={styles['header-logo']}
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
+          alt="Pro Tasker"
         />
         <h2 className={styles['header-title']}>Create your Free Account</h2>
       </div>
 
       <div className={styles['form']}>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit(submitData)}>
           <div className="form-field">
             <label htmlFor="email" className={styles['form-label']}>
               Email address
             </label>
             <div className="mt-2">
-              <input id="email" className={styles['form-input']} />
+              <input id="email" className={styles['form-input']} {...register('email')} />
             </div>
+            {errors?.email?.message && <p className="pt-1.5 text-sm text-red-400">{errors.email.message}</p>}
           </div>
 
           <div className="form-field">
@@ -29,8 +55,9 @@ const RegisterForm = () => {
               Password
             </label>
             <div className="mt-2">
-              <input id="password" type="password" className={styles['form-input']} />
+              <input id="password" type="password" className={styles['form-input']} {...register('password')} />
             </div>
+            {errors?.password?.message && <p className="pt-1.5 text-sm text-red-400">{errors.password.message}</p>}
           </div>
 
           <div className="form-field">
@@ -38,8 +65,16 @@ const RegisterForm = () => {
               Confirm Password
             </label>
             <div className="mt-2">
-              <input id="confirm-password" type="password" className={styles['form-input']} />
+              <input
+                id="confirm-password"
+                type="password"
+                className={styles['form-input']}
+                {...register('confirmPassword')}
+              />
             </div>
+            {errors?.confirmPassword?.message && (
+              <p className="pt-1.5 text-sm text-red-400">{errors.confirmPassword.message}</p>
+            )}
           </div>
 
           <div className="form-actions">
