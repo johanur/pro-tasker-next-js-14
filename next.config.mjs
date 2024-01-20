2a1455bb0033933acd677b2123e851ac4dd2dd23
Johan <johanur.rahman@pm.me>
2024-01-20 12:38:03 +0600

    SASS package added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
 export default nextConfig;
