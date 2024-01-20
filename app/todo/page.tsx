9ff643a79197a07913d6d57e1afd653b506dcce6
Johan <johanur.rahman@pm.me>
2024-01-20 14:50:25 +0600

    Logout functionality added

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,12 +1,18 @@
 import { redirect } from 'next/navigation';
 import readUserSession from '../../lib/actions/user-session.action';
+import Logout from '@/app/todo/components/Logout';
 
 const Todo = async () => {
   const { data } = await readUserSession();
   if (!data.session) {
     return redirect('/auth/login');
   }
-  return <h1>Todos works</h1>;
+  return (
+    <>
+      <h1>Todos works</h1>
+      <Logout />
+    </>
+  );
 };
 
 export default Todo;
