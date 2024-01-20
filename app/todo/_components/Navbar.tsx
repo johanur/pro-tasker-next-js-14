5b202bb87138e4d2299b88573201e013c723253c
Johan <johanur.rahman@pm.me>
2024-01-20 17:31:01 +0600

    Navbar component created

---
+++ app/todo/_components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Logout from '@/app/todo/_components/Logout';
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 z-50 flex h-14 w-full items-center justify-between border-b bg-white px-4 shadow-sm">
+      <h1>Pro Tasker</h1>
+      <Logout />
+    </nav>
+  );
+};
+
+export default Navbar;
