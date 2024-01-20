a5b4fdbd3d390ec8b5738b872e73320c43486039
Johan <johanur.rahman@pm.me>
2024-01-20 13:08:47 +0600

    Login and Register forms css class name updated

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,68 +1,51 @@
 import Link from 'next/link';
+import styles from '../styles/auth.module.scss';
 
 const LoginForm = () => {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className={styles['authentication-container']}>
+      <div className={styles.header}>
         <img
-          className="mx-auto h-10 w-auto"
+          className={styles['header-logo']}
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
+          alt="Pro Tasker"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
+        <h2 className={styles['header-title']}>Sign in to your account</h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+      <div className={styles['form']}>
+        <form className="space-y-6">
+          <div className="form-field">
+            <label htmlFor="email" className={styles['form-label']}>
               Email address
             </label>
             <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <input id="email" name="email" type="email" className={styles['form-input']} />
             </div>
           </div>
 
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+          <div className="form-field">
+            <label htmlFor="password" className={styles['form-label']}>
               Password
             </label>
             <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <input id="password" name="password" type="password" className={styles['form-input']} />
             </div>
           </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <div className="form-actions">
+            <button type="submit" className={styles['form-submit-button']}>
               Login
             </button>
           </div>
         </form>
+      </div>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{' '}
-          <Link href="/auth/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-            Register
-          </Link>
-        </p>
+      <div className={styles.footer}>
+        <span className={styles['footer-text']}>Not a member? </span>
+        <Link href="/auth/register" className={styles['footer-action']}>
+          Register
+        </Link>
       </div>
     </div>
   );
--- app/auth/components/RegisterForm.tsx
+++ app/auth/components/RegisterForm.tsx
@@ -1,84 +1,60 @@
 import Link from 'next/link';
+import styles from '../styles/auth.module.scss';
 
 const RegisterForm = () => {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className={styles['authentication-container']}>
+      <div className={styles.header}>
         <img
-          className="mx-auto h-10 w-auto"
+          className={styles['header-logo']}
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Pro Tasker"
+          alt="Your Company"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Create your Free Account
-        </h2>
+        <h2 className={styles['header-title']}>Create your Free Account</h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+      <div className={styles['form']}>
+        <form className="space-y-6">
+          <div className="form-field">
+            <label htmlFor="email" className={styles['form-label']}>
               Email address
             </label>
             <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <input id="email" className={styles['form-input']} />
             </div>
           </div>
 
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+          <div className="form-field">
+            <label htmlFor="password" className={styles['form-label']}>
               Password
             </label>
             <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <input id="password" type="password" className={styles['form-input']} />
             </div>
           </div>
 
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+          <div className="form-field">
+            <label htmlFor="confirm-password" className={styles['form-label']}>
               Confirm Password
             </label>
             <div className="mt-2">
-              <input
-                id="confirm-password"
-                name="confirm-password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+              <input id="confirm-password" type="password" className={styles['form-input']} />
             </div>
           </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <div className="form-actions">
+            <button type="submit" className={styles['form-submit-button']}>
               Register
             </button>
           </div>
         </form>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Already a member?{' '}
-          <Link href="/auth/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+        <div className={styles.footer}>
+          <span className={styles['footer-text']}>Already a member? </span>
+          <Link href="/auth/login" className={styles['footer-action']}>
             Login
           </Link>
-        </p>
+        </div>
       </div>
     </div>
   );
