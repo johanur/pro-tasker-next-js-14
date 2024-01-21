468d6156fbc4ff37bee919d722efea0d3e3538ff
Johan <johanur.rahman@pm.me>
2024-01-21 19:39:51 +0600

    null clause added for in root board page

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,15 +1,19 @@
 import { redirect } from 'next/navigation';
+import { getCategoriesWithTodos } from '@/app/board/_actions';
 import readUserSession from '@/lib/actions/user-session.action';
 import CategoryContainer from './_components/category/CategoryContainer';
-import { getCategoriesWithTodos } from '@/app/board/_actions/board.actions';
 
 const Todo = async () => {
   const { data: userData } = await readUserSession();
   if (!userData.session) {
     return redirect('/login');
   }
+
   const { data: categoriesWithTodos } = await getCategoriesWithTodos();
-  console.log('Log Here categories:', categoriesWithTodos);
+
+  if (!categoriesWithTodos) {
+    return <h1>Something went wrong!</h1>
+  }
 
   return (
     <div className="h-full overflow-x-auto p-4">
