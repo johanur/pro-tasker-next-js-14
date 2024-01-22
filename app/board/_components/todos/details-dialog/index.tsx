dced3bb670912abdd3969d38af92a88d3b39272c
Johan <johanur.rahman@pm.me>
2024-01-22 22:27:33 +0600

    Redundant check for todo removed in todo details

--- app/board/_components/todos/details-dialog/index.tsx
+++ app/board/_components/todos/details-dialog/index.tsx
@@ -1,13 +1,14 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { getDaysRemaining } from '@/app/board/_utils';
+import { DescriptionRef, ExtendedTodo, Todo, TodoDetailsProps } from '@/app/board/_types';
 import Header from './Header';
 import Description from './Description';
 import CategorySelect from './CategorySelect';
 import ExpiryDatepicker from '@/app/board/_components/todos/details-dialog/ExpiryDatepicker';
-import { useEffect, useRef, useState } from 'react';
-import { DescriptionRef, Todo } from '@/app/board/_types';
-import { getDaysRemaining } from '@/app/board/_utils';
 
-const TodoDetails = ({ isOpen, onToggle, todo, categoryTitle }: any) => {
+const TodoDetails = ({ isOpen, onToggle, todo }: TodoDetailsProps) => {
   const [todoDetails, setTodoDetails] = useState(todo);
   const descriptionRef = useRef<DescriptionRef>();
 
@@ -16,7 +17,7 @@ const TodoDetails = ({ isOpen, onToggle, todo, categoryTitle }: any) => {
   }, [todo]);
 
   const handleTodoUpdate = (updatedTodo: Todo) => {
-    const todoDetails = {
+    const todoDetails: ExtendedTodo = {
       ...updatedTodo,
       daysRemaining: getDaysRemaining(updatedTodo.expire_date),
     };
@@ -32,10 +33,10 @@ const TodoDetails = ({ isOpen, onToggle, todo, categoryTitle }: any) => {
     <>
       <Dialog open={isOpen} onOpenChange={handleCloseDialog}>
         <DialogContent className="max-w-xl gap-4" onOpenAutoFocus={e => e.preventDefault()}>
-          {todo && <Header todo={todoDetails} handleTodoUpdate={handleTodoUpdate} />}
-          {todo && <CategorySelect todo={todoDetails} handleTodoUpdate={handleTodoUpdate} />}
-          {todo && <ExpiryDatepicker todo={todoDetails} handleTodoUpdate={handleTodoUpdate} />}
-          {todo && <Description ref={descriptionRef} todo={todoDetails} handleTodoUpdate={handleTodoUpdate} />}
+          <Header todo={todoDetails} handleTodoUpdate={handleTodoUpdate}   />
+          <CategorySelect todo={todoDetails} handleTodoUpdate={handleTodoUpdate}  />
+          <ExpiryDatepicker todo={todoDetails} handleTodoUpdate={handleTodoUpdate}  />
+          <Description ref={descriptionRef} todo={todoDetails} handleTodoUpdate={handleTodoUpdate} />
         </DialogContent>
       </Dialog>
     </>
