e599bbf542201123521d4e9f62164cf77c5a8525
Johan <johanur.rahman@pm.me>
2024-01-22 20:01:15 +0600

    Utils function added to getUpdatedCategory after drag and drop

--- app/board/_utils/index.ts
+++ app/board/_utils/index.ts
@@ -1,4 +1,5 @@
 import { differenceInCalendarDays, parseISO } from 'date-fns';
+import { CategoryWithTodos } from '@/app/board/_types';
 
 export const getDaysRemaining = (expiryDate: string): number => {
   const parsedExpiryDate = parseISO(expiryDate);
@@ -6,3 +7,40 @@ export const getDaysRemaining = (expiryDate: string): number => {
   const differenceInDays = differenceInCalendarDays(parsedExpiryDate, currentDate);
   return differenceInDays < 0 ? -1 : differenceInDays;
 };
+
+export const getUpdatedCategory = (categoriesWithTodos: CategoryWithTodos[], todoId: string, targetCategoryId: string): CategoryWithTodos[] | undefined => {
+  const sourceCategory = categoriesWithTodos.find(category => category.todos.find(todo => todo.id === todoId));
+  if (!sourceCategory) {
+    return;
+  }
+
+  if (sourceCategory.id === targetCategoryId) {
+    return;
+  }
+
+  const todoToMove = sourceCategory.todos.find(todo => todo.id === todoId);
+
+  if (!todoToMove) {
+    return;
+  }
+
+  const updateSourceCategoryTodos = sourceCategory.todos.filter(todo => todo.id !== todoId);
+
+  const targetCategory = categoriesWithTodos.find(category => category.id === targetCategoryId);
+
+  if (!targetCategory) {
+    return;
+  }
+
+  const updatedTodoToMove = { ...todoToMove, category_id: targetCategoryId };
+  const updatedTargetTodos = [...targetCategory.todos, updatedTodoToMove];
+
+  return categoriesWithTodos.map(category => {
+    if (category.id === sourceCategory.id) {
+      return { ...category, todos: updateSourceCategoryTodos };
+    } else if (category.id === targetCategory.id) {
+      return { ...category, todos: updatedTargetTodos };
+    }
+    return category;
+  });
+}
