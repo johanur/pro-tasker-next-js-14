a66a81bee4d0a22bb1a40f260ba5738279724078
Johan <johanur.rahman@pm.me>
2024-01-22 15:01:56 +0600

    Navbar text updated

--- app/board/_components/Navbar.tsx
+++ app/board/_components/Navbar.tsx
@@ -3,7 +3,7 @@ import Logout from '@/app/board/_components/Logout';
 const Navbar = () => {
   return (
     <nav className="fixed top-0 z-50 flex h-14 w-full items-center justify-between border-b bg-white px-4 shadow-sm">
-      <h1>Pro Tasker</h1>
+      <h1 className="text-lg text-neutral-700 font-semibold">ProTasker</h1>
       <Logout />
     </nav>
   );
