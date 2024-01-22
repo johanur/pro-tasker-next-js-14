f09c8ba684e85bcbc80a2935dff95ae2b1321962
Johan <johanur.rahman@pm.me>
2024-01-22 16:34:21 +0600

    Index form schema file added for login and registration form

---
+++ app/(auth)/_schema/index.ts
@@ -0,0 +1,24 @@
+import { z, ZodType } from 'zod';
+import { LoginFormData, RegisterFormData } from '../_types/form';
+
+export const LoginSchema: ZodType<LoginFormData> = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'Email is required' })
+    .email('Invalid email address. Please enter a valid email address'),
+  password: z.string().min(1, { message: 'Password is required' }),
+});
+
+export const RegisterSchema: ZodType<RegisterFormData> = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: 'Email is required' })
+      .email('Invalid email address. Please enter a valid email format (e.g., example@example.com)'),
+    password: z.string().min(6, { message: 'Password must be at least 6 characters.' }),
+    confirmPassword: z.string(),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'Password and Confirm Password do not match',
+    path: ['confirmPassword'], // path of error
+  });
